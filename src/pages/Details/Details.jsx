1921b9bba1de2b923cffd906cb1529e11ae98ae5
ChrissQAng <goodomen@outlook.de>
2024-03-21 15:34:26 +0100

    details start

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import "./Details.css";
 
 const Details = () => {
+  // --- useState
+  const [singleGame, setSingleGame] = useState([]);
+
+  // ---fetch
+  useEffect(() => {
+    fetch("https://www.freetogame.com/api/game?id=452")
+      .then((res) => res.json())
+      .then((fetchData) => setSingleGame(fetchData))
+      .catch((err) => console.error("fetch error at detailpage", err));
+  }, []);
+
   return (
     <section className="details">
       <h2>Details</h2>
