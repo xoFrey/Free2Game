36d86a4ac89bb357d8985d74b533e493ff3c39c4
Reazy96 <resul.sadriu@outlook.com>
2024-03-22 16:27:25 +0100

    reazy commit

--- src/pages/AllGames/AllGames.jsx
+++ src/pages/AllGames/AllGames.jsx
@@ -2,11 +2,14 @@ import "./AllGames.css";
 import Filter from "./../../components/Filter/Filter";
 import CardVertical from "./../../components/CardVertical/CardVertical";
 import Title from "../../components/Title/Title";
+import allGamesImage from "../../../public/img/allGames.jpg";
+
 const AllGames = () => {
   return (
     <section className="all-games">
       <h2>All Games</h2>
-      <Title url="../../../public/img/allGames.jpg" title="All Games" />
+
+      <Title backgroundImage={allGamesImage} title={"All Games"} />
       <Filter />
       <CardVertical />
       {/* daten.map((item)=> (
