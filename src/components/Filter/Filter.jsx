a73f7e884d878cf9344b1f310879264ca010cca7
xoFrey <xizelacar@gmail.com>
2024-03-22 16:55:36 +0100

    udj

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,13 @@
 import "./Filter.css";
-import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ArrowIcon from "./../../assets/svg/ArrowIcon";
 const Filter = () => {
   const [drop, setDrop] = useState(true);
   console.log(drop);
+
+  const butee = () => {
+    console.log("halloooo");
+  };
 
   return (
     <section className="filter">
@@ -11,11 +15,15 @@ const Filter = () => {
       <div className="platform">
         <div className="drop-down">
           <h3>Platform</h3>
-          <button onClick={() => setDrop(false)} type="button">
-            Click me
-          </button>
+          {/* <ArrowIcon setDrop={() => setDrop((drop) => !drop)} /> */}
+          {/* <button onClick={() => setDrop((drop) => !drop)}>frffrf</button> */}
+          <button onClick={butee}>frffrf</button>
         </div>
       </div>
+      <select>
+        <option value="someOption">Some option</option>
+        <option value="otherOption">Other option</option>
+      </select>
     </section>
   );
 };
