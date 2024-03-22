30d0a05586a640cea611379cc27bb963686474c4
Nostea <kiwiuchi@googlemail.com>
2024-03-22 15:07:04 +0100

    finalized styling of Top 4 Pc Games section

--- src/components/TopGamesPC/TopGamesPC.jsx
+++ src/components/TopGamesPC/TopGamesPC.jsx
@@ -1,14 +1,42 @@
 import "./TopGamesPC.css";
 import TopCard from "./../TopCard/TopCard";
 import CardHorizontal from "./../CardHorizontal/CardHorizontal";
+import { useEffect, useState } from "react";
 const TopGamesPC = () => {
+  const [topGameData, setTopGameData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.freetogame.com/api/games?sort-by=popularity?platform=browser")
+      .then((res) => res.json())
+      .then((apiData) => setTopGameData(apiData))
+      .catch((error) => console.log("Error in TopGamesPC.jsx fetch", error));
+  }, []);
+  console.log(topGameData);
+
   return (
-    <section className="top-games-pc">
+    <section className="pc">
       <h2>Top 4 Games for PC in March 2024</h2>
-      <div className="top-games-pc-flex-bigCard">
-        <TopCard gameTitle="Game Title" />
+      <div className="top-component-pc-flex">
+        {topGameData ? (
+          topGameData.slice(0, 1).map((item, index) => (
+            <div className="top-card" key={index} style={{ backgroundImage: `url(${item.thumbnail})`, backgroundPosition: "center", backgroundSize: "cover" }}>
+              <TopCard gameTitle={item.title} imgURL={item.thumbnail} tags={item.genre} id={`/details/${item.id}`} />
+            </div>
+          ))
+        ) : (
+          <p>loading</p>
+        )}
+
         <div className="top-games-pc-flex-vertCards">
-          <CardHorizontal />
+          {topGameData ? (
+            topGameData.slice(1, 4).map((item, index) => (
+              <div key={index}>
+                <CardHorizontal imgURL={item.thumbnail} gameTitle={item.title} tags={item.genre} rankIndex={index} />
+              </div>
+            ))
+          ) : (
+            <p>loading</p>
+          )}
         </div>
       </div>
     </section>
