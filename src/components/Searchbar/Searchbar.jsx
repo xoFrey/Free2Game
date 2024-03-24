c973a8dd095af77fdd53be0840036a8dcf2da27f
Reazy96 <resul.sadriu@outlook.com>
2024-03-24 21:02:17 +0100

    lightmode

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,28 +3,34 @@ import Logo from "./../../assets/svg/Logo";
 import SearchIcon from "./../../assets/svg/SearchIcon";
 import { useContext } from "react";
 import { UserInput } from "../Context/Context";
+import Lightmode from "../Lightmode/Lightmode";
 const Searchbar = ({ fold }) => {
   const { userInput, setUserInput } = useContext(UserInput);
 
   return (
-    <section className="searchbar">
-      <div className={fold ? "slide" : ""}>
-        <Logo />
-        <h2>Free2Game</h2>
-      </div>
-      <div className="user-input">
-        <SearchIcon />
-        <input
-          onChange={(e) => setUserInput(e.target.value)}
-          value={userInput}
-          type="text"
-          name="search"
-          id="search"
-          placeholder=""
-          autoComplete="off"
-        />
-      </div>
-    </section>
+    <>
+      <section className="searchbar">
+        <div className={fold ? "slide" : ""}>
+          <Logo />
+          <h2>Free2Game</h2>
+        </div>
+        <div>
+          <div className="user-input">
+            <SearchIcon />
+            <input
+              onChange={(e) => setUserInput(e.target.value)}
+              value={userInput}
+              type="text"
+              name="search"
+              id="search"
+              placeholder=""
+              autoComplete="off"
+            />
+          </div>
+          <Lightmode />
+        </div>
+      </section>
+    </>
   );
 };
 
