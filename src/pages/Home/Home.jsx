f21bb3c9c74bad9ac9e583b39104902776172ead
xoFrey <xizelacar@gmail.com>
2024-03-27 11:22:16 +0100

    last push i swear

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,26 @@ import TopGamesPC from "./../../components/TopGamesPC/TopGamesPC";
 import TopGamesBrowser from "./../../components/TopGamesBrowser/TopGamesBrowser";
 import RecentlyComponent from "../../components/RecentlyComponent/RecentlyComponent";
 import { Link } from "react-router-dom";
-import { AllGamesFetch, Recently, SearchedGames, UserInput } from "../../components/Context/Context";
+import {
+  AllGamesFetch,
+  GenreValue,
+  PlatformValue,
+  Recently,
+  SearchedGames,
+  SortByValue,
+  UserInput,
+} from "../../components/Context/Context";
 import { useContext, useEffect } from "react";
 import BackToTop from "../../components/BackToTop/BackToTop";
 
 const Home = () => {
-  const { allGames } = useContext(AllGamesFetch);
+  const { allGames, setAllGames } = useContext(AllGamesFetch);
   const { recentlyFetch, setRecentlyFetch } = useContext(Recently);
   const { userInput, setUserInput } = useContext(UserInput);
   const { searchedGames, setSearchedGames } = useContext(SearchedGames);
+  const { genreValue } = useContext(GenreValue);
+  const { platformValue } = useContext(PlatformValue);
+  const { sortByValue } = useContext(SortByValue);
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -21,11 +32,24 @@ const Home = () => {
   };
 
   useEffect(() => {
-    const filtered = allGames.filter((item) => item.title.toLowerCase().includes(userInput.toLowerCase()));
+    const filtered = allGames.filter((item) =>
+      item.title.toLowerCase().includes(userInput.toLowerCase())
+    );
     setSearchedGames(filtered);
   }, [allGames, userInput]);
 
-  console.log(allGames);
+  useEffect(() => {
+    fetch(
+      `https://www.freetogame.com/api/games?${
+        platformValue != "" ? "platform=" + platformValue : ""
+      }&${genreValue != "" ? "category=" + genreValue : ""}&${
+        sortByValue != "" ? "sort-by=" + sortByValue : ""
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => setAllGames(data))
+      .catch((err) => console.log("All Games Fetch", err));
+  }, [platformValue, genreValue, sortByValue]);
 
   return (
     <>
@@ -45,7 +69,10 @@ const Home = () => {
         <section className="suggestion">
           {searchedGames.map((item) => (
             <Link to={`/details/${item.id}`}>
-              <div onClick={() => setUserInput("")} className="suggestion-items">
+              <div
+                onClick={() => setUserInput("")}
+                className="suggestion-items"
+              >
                 <img src={item.thumbnail} alt="" />
                 <h3>{item.title}</h3>
               </div>
